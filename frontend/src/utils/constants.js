2642f55a7d2424b80665af3cb680241f195e2811
Mayank Mokta <mayankmokta@gmail.com>
2025-12-17 18:44:44 +0530

    error done

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,5 @@
-import EthGPT from "../../../smart_contracts/artifacts/contracts/EthGPT.sol/EthGPT.json";
+// import EthGPT from "../../../smart_contracts/artifacts/contracts/EthGPT.sol/EthGPT.json";
+import EthGPT from "./EthGPT_ABI.json";
 
 export const ETHGPT_ADDRESS = "0x15Bebaee8eECacD2727200a60dA576a941B96802";
 
